1e721d2740368534f555f77ed4dc46245475a8b6
handreygalon <handrey.eg@gmail.com>
2020-03-29 18:07:20 -0300

    # Created HeroesComponent and a simple way to edit the hero name

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    /* Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic. */
+  }
+
+}
