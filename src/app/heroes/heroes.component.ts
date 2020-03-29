6699b51c2a2e800c14e7ade10c7903efd212e298
handreygalon <handrey.eg@gmail.com>
2020-03-29 20:01:49 -0300

    # display a list of heroes, and allow users to select a hero and display the hero's details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,15 +8,17 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES; // Property called heroes to expose the HEROES array for binding
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
     /* Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic. */
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
