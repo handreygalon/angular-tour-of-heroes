c5652cfe3ce173b8ebce99a0fb859f39a75a5b98
handreygalon <handrey.eg@gmail.com>
2020-04-04 12:13:37 -0300

    # Create the MessageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,7 +18,7 @@ export class HeroesComponent implements OnInit {
   to properties. The constructor shouldn't do anything. It certainly shouldn't call a function
   that makes HTTP requests to a remote server as a real data service would.
   */
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     /*
@@ -28,6 +30,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
